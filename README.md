8350bff0e40fc8f72dd8812ebd536249f930eea4
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2019-12-14 10:27:38 +0530

    important todo

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ A distributed methodology to search files in a system.
 
 * Phase 1: Design network topology
 * Phase 2: Design & Develop a socket-based solution to find files requested by different nodes.
-* Web service (REST API) to support file transfer.
+* Pase 3: Web service (REST API) to support file transfer.
+
+## Notes
+
+1. After the initial handshakes and housekeeping, the client should connect to other peers (for file sharing). Thus, "host" and "port" should resemble those of that peers. This can be facilitated by giving some sort of a map. Think about it. Or, we should close the connection with the BS and initiate another connection with the peers after the initial handshakes.After the initial handshakes and housekeeping, the client should connect to other peers (for file sharing). Thus, "host" and "port" should resemble those of that peers. This can be facilitated by giving some sort of a map. Think about it. Or, we should close the connection with the BS and initiate another connection with the peers after the initial handshakes. This should be remedied in `UDPClient.java` file in its `run()` method.
--- filesearch/src/main/java/org/realitix/dfilesearch/filesearch/socket/UDPClient.java
+++ filesearch/src/main/java/org/realitix/dfilesearch/filesearch/socket/UDPClient.java
@@ -12,6 +12,13 @@ public class UDPClient {
     private final Logger logger = Logger.getLogger(UDPClient.class);
     private EventLoopGroup group = new NioEventLoopGroup();
 
+    /**
+     * Runs the client socket
+     * @param host server host IP
+     * @param port server port
+     * @return channel connecting the client and the server
+     * TODO: After the initial handshakes and housekeeping, the client should connect to other peers (for file sharing). Thus, "host" and "port" should resemble those of that peers. This can be facilitated by giving some sort of a map. Think about it. Or, we should close the connection with the BS and initiate another connection with the peers after the initial handshakes.
+     */
     public Channel run(String host, int port) {
         Channel channel = null;
         Bootstrap b = new Bootstrap();
@@ -24,7 +31,7 @@ public class UDPClient {
                     }
                 });
         try {
-        channel = b.connect(host, port).sync().channel();
+        channel = b.connect(host, port).sync().channel();   // connects to the server.
         } catch (InterruptedException e) {
             logger.error(e.getMessage());
         } finally {
